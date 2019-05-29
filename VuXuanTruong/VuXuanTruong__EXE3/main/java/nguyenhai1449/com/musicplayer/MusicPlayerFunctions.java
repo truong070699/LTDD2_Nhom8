ea11fb745030991883f86953c7c29158f2ddf7c0
truong070699 <43981639+truong070699@users.noreply.github.com>
2019-05-29 16:16:54 +0700

    Add files via upload

---
+++ VuXuanTruong/VuXuanTruong__EXE3/main/java/nguyenhai1449/com/musicplayer/MusicPlayerFunctions.java
@@ -0,0 +1,9 @@
+package nguyenhai1449.com.musicplayer;
+
+public interface MusicPlayerFunctions {
+    void onPreviousClicked();
+    void onPlayClicked();
+    void onPauseClicked();
+    void onStopClicked();
+    void onNextClicked();
+}
